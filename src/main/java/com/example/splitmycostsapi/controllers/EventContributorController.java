e5c40396d3ad30ebc348dcbadce3b9189b8adfe6
Thilan Senanayake <thilansenanayake@gmail.com>
2022-07-17 20:24:08 +0530

    Implemented remaning endpoints for event contributors

--- src/main/java/com/example/splitmycostsapi/controllers/EventContributorController.java
+++ src/main/java/com/example/splitmycostsapi/controllers/EventContributorController.java
@@ -3,11 +3,14 @@ package com.example.splitmycostsapi.controllers;
 import com.example.splitmycostsapi.models.Event;
 import com.example.splitmycostsapi.models.requestmodels.AddContributorRequest;
 import com.example.splitmycostsapi.services.EventService;
+import com.example.splitmycostsapi.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EventContributorController {
@@ -15,9 +18,36 @@ public class EventContributorController {
     @Autowired
     private EventService eventService;
 
+    @GetMapping("/events/{eventId}/contributors")
+    public List<UserEntity> getEventContributors(@PathVariable Long eventId){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = (User) authentication.getPrincipal();
+
+        Event event = eventService.getEventById(eventId).orElseThrow();
+
+        return event.getContributors();
+    }
+
     @PostMapping("/events/{eventId}/contributors")
     public Event addContributor(@PathVariable Long eventId, @RequestBody AddContributorRequest addContributorRequest){
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = (User) authentication.getPrincipal();
+
         return eventService.addContributor(eventId, addContributorRequest.getContributorId());
+    }
+
+    @DeleteMapping("/events/{eventId}/contributors/{contributorId}")
+    public Event removeContributor(
+            @PathVariable Long eventId, @PathVariable Long contributorId
+    ){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User user = (User) authentication.getPrincipal();
+
+        return eventService.removeContributor(eventId, contributorId);
+
     }
 }
